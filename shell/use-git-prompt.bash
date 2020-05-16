c43dbcac2e2af1241f7c6bd127910ec222a3e45a
Jason Cox <dev@jasoncarloscox.com>
2020-05-15 23:53:53 -0400

    Use fancy prompt with symbols and git info

--- shell/use-git-prompt.bash
+++ shell/use-git-prompt.bash
@@ -5,9 +5,28 @@
 # already with git completion, but I include my own tweaked version.
 [[ -f ~/dotfiles/shell/git-prompt.sh ]] && . ~/dotfiles/shell/git-prompt.sh
 
-PROMPT_PRE_GIT="[\[\e[36m\]\W\[\e[0m\]"
-PROMPT_POST_GIT="]\\$ "
+PROMPT_DIRTRIM=2 # only show two deepest dirs with '\w'
 
+__prompt_pre_git() {
+    local EXIT_CODE="$?"
+    local DEFAULT='\[\e[m\]'
+    local RED='\[\e[31;1m\]'
+    local GREEN='\[\e[32;1m\]'
+    local CYAN='\[\e[36m\]'
+
+    # determine exit msg based on exit code
+    if [ "$EXIT_CODE" != 0 ]; then
+        local exit_msg="$RED✖ $DEFAULT"
+    else
+        local exit_msg="$GREEN✔ $DEFAULT"
+    fi
+
+    echo "$exit_msg[ 🖿  $CYAN\w$DEFAULT"
+}
+
+PROMPT_POST_GIT=" ] \\$ "
+
+# use git prompt if available
 if command -v __git_ps1 &> /dev/null; then
     GIT_PS1_SHOWDIRTYSTATE="yes"
     GIT_PS1_SHOWSTASHSTATE="yes"
@@ -15,8 +34,8 @@ if command -v __git_ps1 &> /dev/null; then
     GIT_PS1_SHOWUPSTREAM="yes"
     GIT_PS1_SHOWCOLORHINTS="yes"
     GIT_PS1_STATESEPARATOR=""
-    PROMPT_COMMAND="__git_ps1 \"$PROMPT_PRE_GIT\" \"$PROMPT_POST_GIT\" \":%s\"" 
+    PROMPT_COMMAND="__git_ps1 \"\$(__prompt_pre_git)\" \"$PROMPT_POST_GIT\" \"  ⎇  %s\""
 else
-    PS1="$PROMPT_PRE_GIT$PROMPT_POST_GIT"
+    PROMPT_COMMAND="PS1=\"\$(__prompt_pre_git)$PROMPT_POST_GIT"\"
 fi
 
